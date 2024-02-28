daa70ed2c9d36cbb7b3cccc83d82d4873a73c11a
varshithaa <varshithab@cybage.com>
2024-02-28 11:43:49 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponentComponent } from './main-component/main-component.component';
+import { HomeComponent } from './main-component/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponentComponent
+    MainComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
